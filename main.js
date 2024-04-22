a1d9895dc70ccdf701209876a2d11d53d3d4bb20
Giuseppe Cattano <giuseppe.cattano@gmail.com>
2024-04-22 21:01:25 +0200

    completato esercizio operatori 3

---
+++ main.js
@@ -0,0 +1,8 @@
+let num1 = Number(prompt(`Inserisci il primo numero: `));
+let num2 = Number(prompt(`Inserisci il secondo numero: `));
+let somma = num1 + num2;
+let sottrazione = num1 - num2;
+let moltiplicazione = num1 * num2;
+let divisione = num1 / num2;
+let potenza = num1 ** num2;
+console.log(`Con i numeri da te scelti, i risultati delle varie operazioni sono: somma:${somma}, sottrazione:${sottrazione}, moltiplicazione:${moltiplicazione}, divisione:${divisione}, potenza:${potenza}.`)
